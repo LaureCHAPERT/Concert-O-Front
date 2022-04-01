ba9155a02e5595407c035d995d4f62b068cf4513
Elise-Pelletier <pelletier.elise36@gmail.com>
2022-04-01 18:09:47 +0200

    reactBootstrap - test think about id

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,3 +1,4 @@
+import './style.scss';
 import {
   Container,
   Navbar,
@@ -5,22 +6,25 @@ import {
 } from 'react-bootstrap';
 import logo from 'src/assets/images/logo.png';
 
-import './navBar.scss';
 
 const AppHeader = () => (
-  <Navbar bg="light" expand="lg">
-    <Container>
-      <Navbar.Brand href="#home"> <img src={logo} alt="logo" height="80" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link className="nav-link" href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+  <div>
+    <Navbar id="mainNav" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home"> <img src={logo} alt="logo" height="80" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link id="nav-link" href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <h1>Test CSS</h1>
+  </div>
+
 );
 
 export default AppHeader;
