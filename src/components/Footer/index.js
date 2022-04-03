ea645df02cbe70bf517210511b1cbd4313d5563b
LaureCHAPERT <laure.chapert@gmail.com>
2022-04-03 13:15:18 +0200

    fix spacing on footer with flex-grow

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => (
   <div>
     <Navbar id="mainNav" expand="lg">
       <Container id="navbar-container">
-        <Nav>
+        <Nav className="flex-grow-1 justify-content-evenly">
           <Nav.Link href="#home">Contact</Nav.Link>
           <Nav.Link href="#home">Mentions légales</Nav.Link>
         </Nav>
