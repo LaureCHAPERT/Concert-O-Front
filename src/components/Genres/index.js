693720d1ddf127588b814b65fba6080a0b1c09c1
LaureCHAPERT <laure.chapert@gmail.com>
2022-04-04 16:50:22 +0200

    responsive cards p, categories pages

--- src/components/Genres/index.js
+++ src/components/Genres/index.js
@@ -13,16 +13,38 @@ const Genres = () => (
 
   <div className="cardGrid">
     <Row xs={1} md={2} lg={4} className="g-4">
-      {Array.from({ length: 8 }).map((_, idx) => (
-        <Col>
-          <Card style={{ width: '17rem' }} href="#">
-            <Card.Img variant="top" src={logo} />
-            <Card.Body>
-              <Card.Title>Titre</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
+      <Col className="d-flex justify-content-center">
+        <Card style={{ width: '17rem' }} href="#">
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Titre</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <Card style={{ width: '17rem' }} href="#">
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Titre</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <Card style={{ width: '17rem' }} href="#">
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Titre</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col className="d-flex justify-content-center">
+        <Card style={{ width: '17rem' }} href="#">
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>Titre</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
     </Row>
   </div>
 );
--- src/components/Regions/index.js
+++ src/components/Regions/index.js
@@ -13,8 +13,26 @@ import {
 const Regions = () => (
   <div className="cardGrid">
     <Row xs={1} md={2} lg={3} className="g-4">
-      <Col>
+      <Col className="d-flex justify-content-center">
         <CardGroup>
+          <Card style={{ width: '17rem' }} href="#">
+            <Card.Img variant="top" src={logo} />
+            <Card.Body>
+              <Card.Title>Titre</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '17rem' }} href="#">
+            <Card.Img variant="top" src={logo} />
+            <Card.Body>
+              <Card.Title>Titre</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: '17rem' }} href="#">
+            <Card.Img variant="top" src={logo} />
+            <Card.Body>
+              <Card.Title>Titre</Card.Title>
+            </Card.Body>
+          </Card>
           <Card style={{ width: '17rem' }} href="#">
             <Card.Img variant="top" src={logo} />
             <Card.Body>
