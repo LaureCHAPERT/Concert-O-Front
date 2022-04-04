d989919a6e215e5c609781cf9d77af2a149f175d
LaureCHAPERT <laure.chapert@gmail.com>
2022-04-04 19:19:28 +0200

    Fixed links on homepage with stretched link class

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -3,21 +3,22 @@ import {
   Card,
   Button,
 } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const EventCard = () => (
-
-  <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-      </Card.Text>
-      <Button variant="primary">Voir le détail</Button>
-    </Card.Body>
-  </Card>
-
+  <LinkContainer to="/contact">
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text>
+        <Button variant="primary stretched-link">Voir le détail</Button>
+      </Card.Body>
+    </Card>
+  </LinkContainer>
 );
 
 export default EventCard;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,9 +3,7 @@ import {
   CardGroup,
   Row,
   Col,
-  Nav,
 } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import SearchBar from 'src/components/SearchBar';
 import EventCard from '../EventCard';
 
@@ -16,21 +14,9 @@ const Home = () => (
       <Row className=" d-flex justify-content-center">
         <Col>
           <CardGroup>
-            <LinkContainer to="/contact">
-              <Nav.Link>
-                <EventCard />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/contact">
-              <Nav.Link>
-                <EventCard />
-              </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/contact">
-              <Nav.Link>
-                <EventCard />
-              </Nav.Link>
-            </LinkContainer>
+            <EventCard />
+            <EventCard />
+            <EventCard />
           </CardGroup>
         </Col>
       </Row>
