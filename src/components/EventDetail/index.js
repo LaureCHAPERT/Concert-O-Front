f15f5240b6765a3350e1b197b3a209d33caec53a
LaureCHAPERT <laure.chapert@gmail.com>
2022-04-14 15:53:19 +0200

    badge link on detailEvent

--- src/components/EventDetail/index.js
+++ src/components/EventDetail/index.js
@@ -1,22 +1,39 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './eventDetail.scss';
 import {
   Card,
   Image,
   Badge,
 } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { fetchEventsByGenre, fetchEventsByRegion } from '../../actions/events';
 
 const EventDetail = () => {
   const { eventDetail } = useSelector((state) => state.events);
-
+  console.log(eventDetail);
+  const dispatch = useDispatch();
   return (
 
     <Card id="eventDetail" className="justify-content-center">
       <Image className="detailImg" variant="top" src={eventDetail.image} />
       <div className="">
         <h2 className="title">{eventDetail.name}</h2>
-        <Badge pill bg="secondary">{eventDetail.region.name}</Badge>
-        <Badge pill>{eventDetail.genres[0].name}</Badge>
+        <LinkContainer
+          to="/resultats-evenements"
+          onClick={() => {
+            dispatch(fetchEventsByRegion(eventDetail.region.id));
+          }}
+        >
+          <Badge pill bg="secondary">{eventDetail.region.name}</Badge>
+        </LinkContainer>
+        <LinkContainer
+          to="/resultats-evenements"
+          onClick={() => {
+            dispatch(fetchEventsByGenre(eventDetail.genres[0].id));
+          }}
+        >
+          <Badge pill>{eventDetail.genres[0].name}</Badge>
+        </LinkContainer>
       </div>
       <div className="price">{eventDetail.price}€</div>
       <div className="description">{eventDetail.description}</div>
